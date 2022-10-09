b15e5e61b5eabe6b211d7f6b3c8b3d6c12b665c6
Cameron Ray <52524662+Cameron-Ray@users.noreply.github.com>
2022-10-09 20:02:44 +0200

    Flask server recreated with working functionality

--- microscope-webview/main.py
+++ microscope-webview/main.py
@@ -1,39 +1,30 @@
-# import the necessary packages
-import os
+#Import necessary libraries
+import cv2
+from flask import Flask, Response, render_template
 
-from flask import (Flask, Response, render_template, request,
-                   send_from_directory)
-from imutils.video.pivideostream import PiVideoStream
-
-from camera import VideoCamera
-
-pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
-
-# App Globals (do not edit)
+#Initialize the Flask app
 app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 @app.route('/')
 def index():
-    return render_template('index.html') #you can customze index.html here
-
-def gen(camera):
-    #get camera frame
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(pi_camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# Take a photo when pressing camera button
-# @app.route('/picture')
-# def take_picture():
-#     pi_camera.take_picture()
-#     return "None"
-
-if __name__ == '__main__':
-
-    app.run(host='192.168.0.74', debug=False)
+if __name__ == "__main__":
+    app.run(debug=True)
